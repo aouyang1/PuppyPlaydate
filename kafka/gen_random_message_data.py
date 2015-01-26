e0b1f0d5a01605a733b0d1ce4e8294f791230fbb
gy8 <garry.yangguang@gmail.com>
2015-01-26 09:53:19 -0800

    Make feedbacks for kafka part

--- kafka/gen_random_message_data.py
+++ kafka/gen_random_message_data.py
@@ -9,16 +9,23 @@ import json
 
 
 def parse_county_list():
+    # make the file name (here county_list.txt) as input to this function
+    # so it is reusable in the future?
     cnt = 0
+    # spell out the full name "counter" for easier time reading it
     cs_list = []
+    # use full words for cs (counties?) for more readability
     with open('county_list.txt') as f:
         for line in f:
             if 6 <= cnt <= 45085:
+                # this is kinda ghetto, is there a better way to trigger this?
                 split_line = line.split('|')
 
                 county_state = split_line[3]
+                # put the above two lines into one for better readability
 
                 cs_list.append([ls.strip() for ls in county_state.split(',')])
+                # whats ls?
 
             cnt += 1
 
@@ -26,6 +33,7 @@ def parse_county_list():
 
 
 num_messages = 10000000
+# use all caps for constants?
 
 cs_list = parse_county_list()
 
@@ -41,20 +49,31 @@ end_ts = time.mktime(end_dtt)
 for rep in range(25):
     msg_data = []
     f = open('random_messages_' + str(rep) + '.txt', 'w')
+    # use .format()
     for i in range(num_messages):
         rand_ts = random.random()*(end_ts - start_ts) + start_ts
 
         msg = {"county": cs_list[np.random.randint(num_counties)][0],
                "rank": 0,
+               # line length should be less than 80 characters (you have 92)
                "timestamp": list(datetime.datetime.fromtimestamp(rand_ts).timetuple()[0:6]),
                "creatorID": np.random.randint(num_users),
                "messageID": i,
                "senderID": np.random.randint(num_users),
                "message": "".join([random.choice(string.letters) for i in xrange(15)])}
+        # why u using python2 :P?
+        # line length should be less than 80 characters (you have 87)
+        # i think you should make msg into a separate function it would be more
+        # clear.
 
         f.write(json.dumps(msg) + "\n")
+        # use .format() brehhh
         if i % 100000 == 0:
             print i/float(num_messages)
 
     f.close()
     print "DONE!"
+
+# after more modularization wrap the main executable def in
+# __name__ = "__main__" so the functions you write can get exported to be used
+# in other codes or for testing
--- kafka/kafka_to_hdfs_messages.py
+++ kafka/kafka_to_hdfs_messages.py
@@ -17,27 +17,27 @@ class Consumer(object):
 
     def flush_to_hdfs(self, output_dir):
         self.tempfile.close()
-       
+
         timestamp = time.strftime('%Y%m%d%H%M%S')
- 
+
         hadoop_path = "/user/PuppyPlaydate/kafka/%s_%s_%s.dat" % (self.group, self.topic, timestamp)
         print "Block " + str(self.block_cnt) + ": Flushing 20MB file to HDFS => " + hadoop_path
         self.block_cnt += 1
 
         os.system("sudo -u hdfs hdfs dfs -put %s %s" % (self.tempfile_path, hadoop_path))
-       
+
         os.remove(self.tempfile_path)
-        
+
         timestamp = time.strftime('%Y%m%d%H%M%S')
 
         self.tempfile_path = "/home/ubuntu/PuppyPlaydate/kafka/kafka_messages/kafka_%s_%s_%s.dat" % (self.topic, self.group, timestamp)
         self.tempfile = open(self.tempfile_path,"w")
 
     def consume_topic(self, output_dir):
- 
+
         #get timestamp
         timestamp = time.strftime('%Y%m%d%H%M%S')
-        
+
         #open file for writing
         self.tempfile_path = "/home/ubuntu/PuppyPlaydate/kafka/kafka_messages/kafka_%s_%s_%s.dat" % (self.topic, self.group, timestamp)
         self.tempfile = open(self.tempfile_path,"w")
@@ -45,19 +45,19 @@ class Consumer(object):
 	block_cnt = 0
         while True:
             messages = self.consumer.get_messages(count=1000, block=False) #get 1000 messages at a time, non blocking
-            
+
             for message in messages: #OffsetAndMessage(offset=43, message=Message(magic=0, attributes=0, key=None, value='some message'))
                 log_has_at_least_one = True
                 self.tempfile.write(message.message.value + "\n")
-           
+
             if self.tempfile.tell() > 20000000: #file size > 20MB
                 self.flush_to_hdfs(output_dir)
-            
+
             self.consumer.commit()
         #exit loop
         if log_has_at_least_one:
             self.flush_to_hdfs(output_dir, self.topic)
-            
+
         self.consumer.commit() #save position in the kafka queue
         return 0
 
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     group           = "hdfs"
     output          = "/data"
     topic           = "hdfs"
-    
+
     print "\nConsuming topic: [%s] into HDFS" % topic
     cons = Consumer(addr="localhost:9092", group="hdfs", topic="messages")
     cons.consume_topic("user/PuppyPlaydate/kafka")
