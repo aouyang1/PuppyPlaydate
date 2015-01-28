bdbfe5dcc630be745d07cbe2f40bd26aa419565c
Ubuntu <ubuntu@ip-172-31-13-181.us-west-1.compute.internal>
2015-01-28 05:03:32 +0000

    Got spark streaming to compute total number of messages in the last 5 seconds every 5 seconds and write to cassandra table: by_county_rt

--- spark/scala_stream/src/main/scala/stream_msgcnt.scala
+++ spark/scala_stream/src/main/scala/stream_msgcnt.scala
@@ -7,14 +7,14 @@ import org.json4s.jackson.JsonMethods._
 import org.json4s.JsonDSL._
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.streaming._
 
 object StreamExample {
   def main(args: Array[String]) {
 
-    Logger.getLogger("CheckpointWriter").setLevel(Level.ERROR)
-
-    val conf = new SparkConf().setAppName("stream_example")
-    val ssc = new StreamingContext(conf, Seconds(1))
+    val conf = new SparkConf().setAppName("stream_example").set("spark.cassandra.connection.host", "54.215.184.69")
+    val ssc = new StreamingContext(conf, Seconds(5))
        
     ssc.checkpoint("/user/PuppyPlaydate/spark_streaming")
 
@@ -23,18 +23,20 @@ object StreamExample {
     val topics = Map("messages" -> 1)
     val kafkaStream = KafkaUtils.createStream(ssc, zkQuorum, groupID, topics)
 
-    //val window_count = kafkaStream.countByWindow(Seconds(10), Seconds(2))
-    val parsed_message = kafkaStream.map( x => parse(x._2) )
+
+    val parsed_message = kafkaStream.map( x => parse(x._2) ).cache()
+    
     val messages_by_county = parsed_message.map( message => (compact(render( message \ "state" )) + "," + compact(render( message \ "county" )), 1) )
-    val window_count_10sec = messages_by_county.reduceByKeyAndWindow( (a:Int, b:Int) => a + b, Seconds(10), Seconds(1) )
+    val reduced_messages_by_county = messages_by_county.reduceByKey( _ + _ )
+    val tup_messages_by_county = reduced_messages_by_county.map( tup => {val state_county = tup._1.split(",")
+ 								        (state_county(0).tail.dropRight(1), state_county(1).tail.dropRight(1), tup._2)})
     
-    val mapped_window_count_10sec = window_count_10sec.map( tup => {val state_county = tup._1.split(",")
- 								   (System.currentTimeMillis, state_county(0).tail.dropRight(1), state_county(1).tail.dropRight(1), tup._2)}) 
+    //val messages_by_state = parsed_message.map( message => (compact(render( message \ "state" )).tail.dropRight(1), 1) )
+    //val tup_messages_by_state = messages_by_state.reduceByKey( _ + _ )
 
-    // look for minimum timestamp and remap that tuple section
-    mapped_window_count_10sec.print()
-    
 
+    tup_messages_by_county.saveToCassandra("puppy", "by_county_rt", SomeColumns("state", "county", "count") )
+    //tup_messages_by_state.saveToCassandra("puppy", "by_state_rt", SomeColumns("state", "count") )
     
     ssc.start()
     ssc.awaitTermination()
