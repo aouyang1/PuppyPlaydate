9687bdc60f10fcaba47fc74253d0d783c7f2152b
Ubuntu <ubuntu@ip-172-31-13-181.us-west-1.compute.internal>
2015-02-04 10:42:55 +0000

    deleted batch_processing folder and renamed

--- batch_processing/incremental_batch.sh
+++
@@ -1,7 +0,0 @@
-FILE_LIST=$(hdfs dfs -ls /user/PuppyPlaydate/cached | awk '{printf $8","}')
-FILE_LIST=${FILE_LIST:0:-1}
-
-spark-submit --class "batch_msgcnt" --master local[2] --jars target/scala-2.10/uber-assembly.jar target/scala-2.10/countmessagebycounty_2.10-1.0.jar $FILE_LIST
-
-FILE_LIST=${FILE_LIST//,/ }
-sudo -u hdfs hdfs dfs -rm $FILE_LIST
--- batch_processing/test_shell.sh
+++
@@ -1,4 +0,0 @@
-
-FILE_LIST=$(hdfs dfs -ls /user/PuppyPlaydate/history | awk '{printf $8","}')
-FILE_LIST=${FILE_LIST:0:-1}
-echo $FILE_LIST
