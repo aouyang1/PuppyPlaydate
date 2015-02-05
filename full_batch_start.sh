215128ebd4038b2e75d0e90cb2f40dbc3c2e6f0a
Ubuntu <ubuntu@ip-172-31-13-181.us-west-1.compute.internal>
2015-02-05 19:28:30 +0000

    added shell script to run full batch

---
+++ full_batch_start.sh
@@ -0,0 +1,2 @@
+spark-submit --class "batch_msgcnt" --master local[2] --jars full_batch_processing/target/scala-2.10/uber-assembly.jar full_batch_processing/target/scala-2.10/countmessagebycounty_2.10-1.0.jar 
+
