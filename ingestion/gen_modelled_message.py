6ddb6196fd5785377d3ad38237698227c930c0c2
Ubuntu <ubuntu@ip-172-31-13-181.us-west-1.compute.internal>
2015-02-09 14:53:06 +0000

    changed replication of messages in gen_modelled_messages from 200-> 1000

--- ingestion/gen_modelled_message.py
+++ ingestion/gen_modelled_message.py
@@ -9,7 +9,7 @@ from faker import Factory
 fake = Factory.create()
 NUM_MESSAGES = 375000
 NUM_USERS = 1000000
-NUM_REPLICATIONS = 200
+NUM_REPLICATIONS = 1000
 hadoop_remote_path = "/user/PuppyPlaydate/history/"
 
 county_state_list = IngUt.parse_county_list("county_list.txt")
